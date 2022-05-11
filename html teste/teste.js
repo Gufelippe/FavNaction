ac1344d97b0f068e6794b9a7d485bd21346a288c
Gufelippe <91915124+Gufelippe@users.noreply.github.com>
2022-05-11 17:10:10 -0300

    commit 1

--- html teste/teste.js
+++ html teste/teste.js
@@ -1,4 +1,6 @@
-function teste() { 
+const { homedir } = require("os");
+
+function teste() {
     let text = document.getElementById("name2");
     if (text.classList.contains("name_teste")) {
         text.style.display = 'block';
@@ -31,5 +33,58 @@ function fechar2() {
 }
 //----------------------------------------//
 
+function open_pag_1() {
+    trocaVisualizacao(1);
+}
+function open_pag_2() {
+    trocaVisualizacao(2);
+    /*
+    let pag1Styles = window.getComputedStyle(document.querySelector('#pag_2'));
 
+    if (pag1Styles.display === "none") {
+        document.getElementById("pag_2").style.setProperty('display', 'block');
 
+        document.getElementById("pag_1").style.setProperty('display', 'none');
+        document.getElementById("pag_3").style.setProperty('display', 'none');
+    }*/
+}
+function open_pag_3() {
+    trocaVisualizacao(3);
+}
+
+function open_home() {
+    trocaVisualizacao("home");
+}
+
+function trocaVisualizacao(alvo) {
+    let pagAlvo = window.getComputedStyle(document.querySelector('#pag_' + alvo));
+
+    if (pagAlvo.display === "none") {
+        document.getElementById("pag_" + alvo).style.setProperty('display', 'block');
+    }
+
+    switch (alvo) {
+
+        case 1:
+            document.getElementById("pag_home").style.setProperty('display', 'none');
+            document.getElementById("pag_2").style.setProperty('display', 'none');
+            document.getElementById("pag_3").style.setProperty('display', 'none');
+            break;
+        case 2:
+            document.getElementById("pag_home").style.setProperty('display', 'none');
+            document.getElementById("pag_1").style.setProperty('display', 'none');
+            document.getElementById("pag_3").style.setProperty('display', 'none');
+            break;
+        case 3:
+            document.getElementById("pag_home").style.setProperty('display', 'none');
+            document.getElementById("pag_1").style.setProperty('display', 'none');
+            document.getElementById("pag_2").style.setProperty('display', 'none');
+            break;
+        case "home":
+            document.getElementById("pag_home").style.setProperty('display', 'block');
+            document.getElementById("pag_1").style.setProperty('display', 'none');
+            document.getElementById("pag_2").style.setProperty('display', 'none');
+            document.getElementById("pag_3").style.setProperty('display', 'none');
+            break;
+    }
+}
